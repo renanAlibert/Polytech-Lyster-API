d2e5da665b6eabd5e630a2a0dddd3878d69b1b9e
Thibaut Meunier <wiirio.meunier@gmail.com>
2020-01-09 17:34:54 +0100

    Integration IHM V1

--- interface/interface/src/views/Home.vue
+++ interface/interface/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <Game/>
+    <img alt="Vue logo" src="../assets/martian.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{gold}} euros</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Game from "@/components/Game.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Game
+  },
+  data() {
+    return {
+      gold:100
+    }
   }
 };
+
+
+
 </script>
