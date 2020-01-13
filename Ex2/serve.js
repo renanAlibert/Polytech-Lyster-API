2a09880d8c02e49ff5c20bdf47b4c0ea08633fe4
Renan <alibert.renan@gmail.com>
2020-01-13 14:51:06 +0100

    Squelette code serve.js 2

--- Ex2/serve.js
+++ Ex2/serve.js
@@ -1,23 +1,37 @@
-// Chargement du module http
-const http = require('http');
-// Definition du port d'écoute
-const port = 3000;
-/*
-    Création du serveur (createServer)
-    Fonction qui gère les requetes
- */
+//Import du module
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    //Réponse du serveur
-    res.end('Mon premier serveur nodejs!!!');
+//Module NodeJs (middleware) qui permet de parser nos requetes
+const bodyParser = require('body-parser');
+// Initialisation du module
+let app = express();
+let messages = [];
+
+//middleware pour parser les requetes
+app.use('/message', bodyParser.urlencoded({extended: true}));
+
+//Homapage - GET Routing -
+app.get('/', (req, res) => {
+    res.end('HomePage');
 });
-// connection du serveur au port d'ecoute
-server.listen(port, (err) => {
-    //check si il y'a pas une erreur pendant le lancement du serveur
-    if (err) {
-        //Afficher l'erreur survenue pendant le lancement
-        return console.log('Erreur: ', err)
-    }
-    // Afficher l'adresse sur serveur
-    console.log(`Serveur: localhost:${port}`)
+app.get('/add', (req, res) => {
+    res.end('add');
 });
+
+// Send Message - POST Routing -
+app.post('/message', (req, res) => {
+    //recuperation du username et du message envoyé au serveur
+    let {username, message } = req.body;
+    console.dir(req.body);
+    //Ajout du message dans la liste des messages
+    messages.push({username, message});
+    //confirmation d'ajout
+    res.send(req.body);
+});
+// liste des Messages - GET Routing -
+app.get('/message', (req, res) => {
+    //Renvoyer la liste de tous les messages
+    res.json(messages);
+});
+// Ecoute sur le port 1234
+app.listen(1234);
