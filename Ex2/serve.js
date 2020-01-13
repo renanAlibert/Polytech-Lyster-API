239789ec69eeed423261c0aed03b04edd4b46508
KevinSorin <kevin.sorin@u-psud.fr>
2020-01-13 16:41:31 +0100

    Dev après la nouvelle demande client (2eme partie de l'exercice)

--- Ex2/serve.js
+++ Ex2/serve.js
@@ -43,12 +43,43 @@ app.get('/message', (req, res) => {
 // Send Message - POST Routing -
 app.post('/tuples', (req, res) => {
     //recuperation du username et du message envoyé au serveur
-    let {val1, val2 } = req.query;
     
-    //Ajout du message dans la liste des messages
-    tuples.push({val1, val2});
-    //confirmation d'ajout
-    res.send("ajoute");
+    let {val1, val2 } = req.query; //Couple de valeurs
+    let sentMessage = 1; //Gestion du message a envoyer à l'issue du traitement du couple d'entier (Pour savoir si on doit l'ajouter dans la liste ou non, selon les dernieres specs).
+
+    if ((Math.sign(val1) == 1 || Math.sign(val1) == 0) && (Math.sign(val2) == 1 || Math.sign(val2) == 0)) {
+        //Ajout du message dans la liste des messages
+        if (tuples.length < 10)
+        {
+            if (parseInt(val1,10) + parseInt(val2,10) <= 10)
+            {
+                tuples.push({val1, val2});
+            }
+            else
+            {
+                sentMessage = 0;
+            }
+        }
+        else
+        {
+            sentMessage = 0;
+        }
+    }
+    else
+    {
+        sentMessage = 0;
+    }
+
+    if (sentMessage == 1)
+    {
+        //confirmation d'ajout
+        res.send("ajoute");
+    }
+    else
+    {
+        //message d'erreur
+        res.send("erreur à l'ajout");
+    }
 });
 
 // liste des Messages - GET Routing -
@@ -65,8 +96,6 @@ app.get('/somme', (req, res) => {
     tuples.forEach(element => somme += parseInt(element.val1,10) + parseInt(element.val2,10));
     res.json(somme);
 });
-
-
 
 // Ecoute sur le port 1234
 app.listen(1234);
